63a5e73aa495d0275c69ae25c82bc5d89b9a6139
hkeper <hkeper1@gmail.com>
2024-11-03 22:08:49 +0400

    Add js fundamentals dir with first section file

---
+++ jsFundamentals/section1.js
@@ -0,0 +1,8 @@
+//Concatination and interpol
+var price = 50
+var product = "gun"
+var concat = "The price of " + product + " is " + price
+var interpolation = `The price of ${product} is ${price}`
+
+console.log(concat)
+console.log(interpolation)
